446c4c3a2ec1cfd1e7826ed3e174ca1fff63bf94
Steven Clontz <steven.clontz@gmail.com>
2019-02-22 12:16:38 -0600

    fix navbar for mobile; change unicode arrows for wider support

--- viewer/src/components/Navbar.tsx
+++ viewer/src/components/Navbar.tsx
@@ -7,14 +7,23 @@ import UserTab from './Layout/UserTab'
 const Navbar = () => (
   <nav className={'navbar navbar-default navbar-static-top'}>
     <div className="container">
-      <Link to="/" className="navbar-brand">π-Base</Link>
-      <ul className="nav navbar-nav">
-        <li><NavLink activeClassName="active" to="/spaces">Spaces</NavLink></li>
-        <li><NavLink activeClassName="active" to="/properties">Properties</NavLink></li>
-        <li><NavLink activeClassName="active" to="/theorems">Theorems</NavLink></li>
-      </ul>
-
-      <UserTab />
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <Link to="/" className="navbar-brand">π-Base</Link>
+      </div>
+      <div id="navbar" className="navbar-collapse collapse">
+        <ul className="nav navbar-nav">
+          <li><NavLink activeClassName="active" to="/spaces">Spaces</NavLink></li>
+          <li><NavLink activeClassName="active" to="/properties">Properties</NavLink></li>
+          <li><NavLink activeClassName="active" to="/theorems">Theorems</NavLink></li>
+        </ul>
+        <UserTab />
+      </div>
     </div>
   </nav>
 )
